fd13073605bc4a109d65b6fd003fa9869c80fafe
Kawan Xavier <kawanxavier1818@gmail.com>
2024-04-04 22:09:32 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { Camera, CameraType, FlashMode } from 'expo-camera';
+import { useEffect, useState } from 'react';
+import ActionButton from './src/components/ActionButton';
+import useShakeDetector from './src/hooks/useShakeDetector';
 
-export default function App() {
+export default function App() {  
+  
+  const [ permission, requestPermission ] = Camera.useCameraPermissions();
+  const [ ligado, setLigado ] = useState(false);
+  const { isShaking } = useShakeDetector(1000);
+
+  useEffect( () => {
+    if (isShaking) {
+      handleToggleCamera();
+    }
+  }, [ isShaking ]);
+
+  const handleToggleCamera = () => {
+    setLigado(!ligado);
+  }
+
+  if (!permission){
+    return <View><Text>Problema de permissao</Text></View>
+  }
+
+  if (!permission.granted){
+    return(
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>Precisamos de acesso a sua camera</Text>
+        <Button onPress={requestPermission} title='Atribuir permissao' />
+      </View>
+    );
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Camera
+          style={styles.camera}
+          type={CameraType.back}
+          flashMode={ligado ? FlashMode.torch : FlashMode.off}
+
+          />       
+      </View>
+      <ActionButton 
+        action={handleToggleCamera}
+        title={ ligado ? 'Desligar lanterna' : 'Ligar lanterna' }
+        />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  camera: {
+    width: 1,
+    height: 1
+  }
 });
